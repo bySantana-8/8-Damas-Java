7eb69002aec3da538c123aebff5bf130845beef3
Pedro Santana <santanavillares@icloud.com>
2024-09-17 00:38:06 +0200

    eliminado archivo sobrante

--- src/App.java
+++
@@ -1,7 +0,0 @@
-public class Damas {
-    public static void main(String[] args) {
-        char[][] tablero = new char[8][8];
-        inicializarTablero(tablero);
-        imprimirTablero(tablero);
-    }
-}
